5b03804a6ba8c0b81d7ee6395bbeb3c77ecbbc3c
dustinsherwood <102439006+dustinsherwood@users.noreply.github.com>
2023-07-18 18:01:06 -0400

    seg changes

--- segmentation.py
+++ segmentation.py
@@ -20,22 +20,12 @@ print(image.GetSize())
 
 def segment_image(image):
     #register image to unmarked atlas
-    #create blank image (stack of 2d arrays) for each region
+    #create a blank image (3d np array)
     #each region has a set of 3d coordinates, created from the atlas
     #for each region, at the atlas coordinates
-    #   pixel value = registered image pixel value at same coordinate
-    #   other pixels are blank, black, or some other special value to indicate the segment boundaries
-    #return set of images, one image for each region
-
-    #a more efficient method:
-    #   create a single blank image (3d array)
     #   each entry is a tuple of two values:
     #   the pixel value of the registered image at the same coordinate
     #   a number to indicate the region of that pixel
-    #pros: more compact, can classify coordinates with guarunteed no overlap, 
-    #   DL and Clustering can use enter image as data rather than seperate segments
-    #cons: have to make custom function to view the segmentations, 
-    #   DL and Clustering more complex?
 
     print("function incomplete")
 
