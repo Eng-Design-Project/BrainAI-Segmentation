cd29578db4e991f9301a6024b371598bc11f5ed3
Kevin Infante <kevininfante90@gmail.com>
2023-07-21 21:20:31 -0400

    added two functions to the data.py file, one that converts a single DCM file to a pixel array (provided its file path as a parameter) and another function that takes a tuple of DCM files as a parameter, and returns a list of corresponding pixel arrays. Also uploaded 3 DCM files to the repo, for the convenience of accessing DCM files to display.

--- data.py
+++ data.py
@@ -22,7 +22,7 @@ ds = dcmread(file_path)
 
 pixarray = ds.pixel_array
 plt.imshow(pixarray, 'gray', origin='lower')
-plt.show()
+# plt.show()
 
 #print("scan 1 size: ",len(ds.pixel_array))
 print("image dimenstions before: ", pixarray.shape) #this prints the dimensions, e.g. (128, 128)
@@ -30,5 +30,49 @@ print("image dimenstions before: ", pixarray.shape) #this prints the dimensions,
 #the code below resizes the image to (224, 224)
 resized_img=resize(pixarray, (224, 224), anti_aliasing=True)
 plt.imshow(resized_img, 'gray', origin='lower')
-plt.show()
+# plt.show()
 print("image dimensions after: ",resized_img.shape)
+
+
+#Save the resized image in PNG format
+output_dir = ""
+output_file = os.path.join(output_dir, file_path.split(".")[0]+".png")
+plt.imsave(output_file, resized_img)
+
+
+# task: making a method that the project core will call to open up a foler and grab a scan
+# the function below converts a single DCM file to a pixel array and displays it with plt
+def open_file(file_path):
+    d = dcmread(file_path)
+    p_array = d.pixel_array
+    img_resize= resize(p_array, (224, 224), anti_aliasing=True) #resizes image to 224x224
+    #plt.imshow(img_resize, 'gray', origin='lower') #(displaying the pixel array is optional)
+    #plt.show()
+
+# Below is an example of a tuple of dcm files, the tuple can be used as a parameter for
+# the function below it
+dcm_files = (
+    os.path.join(current_directory, 'scan1.dcm'),
+    os.path.join(current_directory, 'scan2.dcm'),
+    os.path.join(current_directory, 'scan3.dcm')
+)
+
+
+# the function below converts a tuple of DCM files to a list of pixel arrays and displays it with plt
+# it takes a tuple as its parameter, and returns a list
+def p_arrays(*files):
+    output_list = []
+    for i in files:
+        d = dcmread(i)
+        p_array = d.pixel_array
+        img_resize= resize(p_array, (224,224), anti_aliasing=True)
+        output_list.append(img_resize)
+    return output_list
+
+
+p_array_list = p_arrays(*dcm_files)
+# "p_array_list" is now a list of pixel arrays
+
+# below is an example of accessing a pixel array from the list, and displaying it with plt
+plt.imshow(p_array_list[2], 'gray', origin='lower')
+plt.show()
