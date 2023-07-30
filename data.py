252c57c4fccbc7890ad489510313e1ad956f7806
Kevin Infante <kevininfante90@gmail.com>
2023-07-30 08:15:05 -0400

    added methods to the data module for saving a directory of DCM scans in PNG format, and also a method for returning a list of resized numpy pixel arrays

--- data.py
+++ data.py
@@ -37,6 +37,29 @@ def view_slice_metadata_from_directory(directory):
         image = sitk.ReadImage(filename)
         print(image.GetMetaData("0020|0032"))
 
+#takes all the dcm files in a directory, and returns a list of numpy pixel arrays of dimensions 224x224
+def resize_and_convert_to_3d_image(directory):
+    image=get_3d_image(directory)
+    array = sitk.GetArrayFromImage(image)
+    new_images = []
+    for i in range(0, array.shape[0]):
+        new_images.append(resize(array[i,:,:], (224, 224), anti_aliasing=True))
+    return new_images
+
+#takes all of the dcm files in a directory, and saves them as png files in (string)new_dir
+def save_sitk_3d_img_png(directory, new_dir):
+    #create a directory called new_dir
+    os.mkdir(os.path.join("", new_dir)) 
+    # Get a list of all DICOM files in the directory
+    scan_files = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(".dcm")]
+    # convert each file to a PNG and save it to the directory
+    for i in range(0, len(scan_files)):
+        image = sitk.ReadImage(scan_files[i])
+        png_file = sitk.GetArrayFromImage(image)[0,:,:]
+        output_file=scan_files[i].split("\\")[1]
+        output_file = os.path.join(new_dir+"\\", output_file.split(".")[0]+".png")
+        plt.imsave(output_file, png_file)
+
 # Path to the directory that contains the DICOM files
 directory1 = "scan1"
 directory2 = "scan2"
@@ -53,6 +76,8 @@ view_sitk_3d_image(image2, 10)
 view_slice_metadata_from_directory(directory1)
 view_slice_metadata_from_directory(directory2)
 
+#Save all of the DCM files in directory1 as PNG files in new directory
+##save_sitk_3d_img_png(directory1, "PNGs")
 
 #get the current working directory
 #current_directory = os.getcwd()
