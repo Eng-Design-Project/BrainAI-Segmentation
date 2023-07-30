b53b77710a7adc1da2283e373e694ae6da918ea0
Justin Rivera <124208345+PurpleTuxx2@users.noreply.github.com>
2023-07-30 09:51:45 -0400

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -1,6 +1,119 @@
+#dicom_path2 = "ADNI_007_S_1339_PT_TRANSAXIAL_BRAIN_3D_FDG_ADNI_CTAC__br_raw_20070402142342176_8_S29177_I47688.dcm"
+#dicom_path1 = "ADNI_007_S_1339_PT_TRANSAXIAL_BRAIN_3D_FDG_ADNI_CTAC__br_raw_20070402142342697_9_S29177_I47688.dcm"
+
 import numpy as np
 import matplotlib.pyplot as plt
 import SimpleITK as sitk
+
+# Load DICOM filepaths
+dicom_path1 = "scan2/ADNI_003_S_1059_PT_adni2__br_raw_20071211125948781_11_S43552_I84553.dcm"
+dicom_path2 = "scan2/ADNI_003_S_1059_PT_adni2__br_raw_20071211125949312_18_S43552_I84553.dcm"
+
+# get sitk images
+image1 = sitk.ReadImage(dicom_path1, sitk.sitkFloat32)
+image2 = sitk.ReadImage(dicom_path2, sitk.sitkFloat32)
+
+# Convert SimpleITK images to numpy arrays for displaying
+pixel_array1 = sitk.GetArrayFromImage(image1)
+pixel_array2 = sitk.GetArrayFromImage(image2)
+
+# Print the dimensions of image1 and image2 before resampling
+print("Image 1 - Before Resampling:")
+print("Size:", image1.GetSize())
+print("Spacing:", image1.GetSpacing())
+print("Direction:", image1.GetDirection())
+print()
+
+print("Image 2 - Before Resampling:")
+print("Size:", image2.GetSize())
+print("Spacing:", image2.GetSpacing())
+print("Direction:", image2.GetDirection())
+print()
+
+# Get the size and spacing of image1
+size_image1 = image1.GetSize()
+spacing_image1 = image1.GetSpacing()
+
+# Calculate the new size of image2 based on the spacing of image1
+size_image2 = [int(round(sz*spc1/spc2)) for sz, spc1, spc2 in zip(size_image1, spacing_image1, image2.GetSpacing())]
+
+# Resample image2 to match the size and spacing of image1
+resampler = sitk.ResampleImageFilter()
+resampler.SetSize(size_image1)
+resampler.SetOutputSpacing(spacing_image1)
+resampler.SetOutputDirection(image1.GetDirection())
+resampler.SetOutputOrigin(image1.GetOrigin())
+resampled_image2 = resampler.Execute(image2)
+image2 = resampled_image2
+
+# Print the dimensions of image1 and image2 after resampling
+print("Image 1 - After Resampling:")
+print("Size:", image1.GetSize())
+print("Spacing:", image1.GetSpacing())
+print("Direction:", image1.GetDirection())
+print()
+
+print("Image 2 - After Resampling:")
+print("Size:", image2.GetSize())
+print("Spacing:", image2.GetSpacing())
+print("Direction:", image2.GetDirection())
+print()
+
+# Perform image registration using Demons registration filter
+demons_registration_filter = sitk.DemonsRegistrationFilter()
+final_transform = demons_registration_filter.Execute(image1, image2)
+
+# Explicitly set the transformation type to DisplacementFieldTransform
+final_transform = sitk.DisplacementFieldTransform(final_transform)
+
+# Apply the final transformation to image2
+registered_image = sitk.Resample(image2, image1, final_transform, sitk.sitkLinear, 0.0, sitk.sitkFloat32)
+
+# Convert SimpleITK images to numpy arrays for displaying
+pixel_array1 = sitk.GetArrayFromImage(image1)
+pixel_array2 = sitk.GetArrayFromImage(image2)
+registered_array = sitk.GetArrayFromImage(registered_image)
+
+# Plot the original and registered images
+plt.figure(figsize=(20, 5))
+
+# Plot Image 1 - Before Resampling
+plt.subplot(151)
+plt.imshow(pixel_array1.squeeze(), cmap='gray')
+plt.title("Image 1 - Before Resampling")
+plt.axis('off')
+
+# Plot Image 2 - Before Resampling
+plt.subplot(152)
+plt.imshow(pixel_array2.squeeze(), cmap='gray')
+plt.title("Image 2 - Before Resampling")
+plt.axis('off')
+
+# Plot Image 1 - After Resampling
+plt.subplot(153)
+plt.imshow(pixel_array1.squeeze(), cmap='gray')
+plt.title("Image 1 - After Resampling")
+plt.axis('off')
+
+# Plot Image 2 - After Resampling
+plt.subplot(154)
+plt.imshow(pixel_array2.squeeze(), cmap='gray')
+plt.title("Image 2 - After Resampling")
+plt.axis('off')
+
+# Plot Registered Image
+plt.subplot(155)
+plt.imshow(registered_array.squeeze(), cmap='gray')
+plt.title("Registered Image")
+plt.axis('off')
+
+plt.show()
+
+
+
+
+
+
 
 #registration testing
 #get two similar images
@@ -21,108 +134,13 @@ import SimpleITK as sitk
 #sitk.sitkLinear
 
 
-# Load DICOM filepaths
-dicom_path1 = "ADNI_007_S_1339_PT_TRANSAXIAL_BRAIN_3D_FDG_ADNI_CTAC__br_raw_20070402142342697_9_S29177_I47688.dcm"
-dicom_path2 = "ADNI_007_S_1339_PT_TRANSAXIAL_BRAIN_3D_FDG_ADNI_CTAC__br_raw_20070402142344709_12_S29177_I47688.dcm"
-
-# get sitk images
-image1 = sitk.ReadImage(dicom_path1, sitk.sitkFloat32)
-image2 = sitk.ReadImage(dicom_path2, sitk.sitkFloat32)
-
-# Print the dimensions of image1 and image2 before resampling
-print("Image 1 - Before Resampling:")
-print("Size:", image1.GetSize())
-print("Spacing:", image1.GetSpacing())
-print("Direction:", image1.GetDirection())
-print()
-
-print("Image 2 - Before Resampling:")
-print("Size:", image2.GetSize())
-print("Spacing:", image2.GetSpacing())
-print("Direction:", image2.GetDirection())
-print()
-
-# Get the size and spacing of image1
-size_image1 = image1.GetSize()
-spacing_image1 = image1.GetSpacing()
-
-# Resample image2 to match the size and spacing of image1
-resampler = sitk.ResampleImageFilter()
-resampler.SetSize(size_image1)
-resampler.SetOutputSpacing(spacing_image1)
-resampled_image2 = resampler.Execute(image2)
-image2 = resampled_image2
-
-# Print the dimensions of image1 and image2 after resampling
-print("Image 1 - After Resampling:")
-print("Size:", image1.GetSize())
-print("Spacing:", image1.GetSpacing())
-print("Direction:", image1.GetDirection())
-print()
-
-print("Image 2 - After Resampling:")
-print("Size:", image2.GetSize())
-print("Spacing:", image2.GetSpacing())
-print("Direction:", image2.GetDirection())
-print()
-
-
-# Initialize the registration with an identity transformation
-# Initialize the transformation with an identity transformation
-initial_transform = sitk.CenteredTransformInitializer(image1, 
-                                                      image2, 
-                                                      sitk.Euler3DTransform(), 
-                                                      sitk.CenteredTransformInitializerFilter.GEOMETRY)
-
-
-# Perform image registration
-registration_method = sitk.ImageRegistrationMethod()
-
-# Set the Demons similarity metric
-registration_method.SetMetricAsDemons(10)  # The smoothing parameter can be adjusted, higher values are more smooth
-
-# Set the optimizer
-registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, numberOfIterations=100)
-
-# Set the interpolator
-registration_method.SetInterpolator(sitk.sitkLinear)
-
-# Execute the registration with the initial transform
-final_transform = registration_method.Execute(image1, image2, initial_transform)
-
-# Apply the final transformation to image2
-registered_image = sitk.Resample(image1, image2, final_transform, sitk.sitkLinear, 0.0, sitk.sitkFloat32)
-
-# Convert SimpleITK imags to numpy arrays for displaying and atlas segmentation
-pixel_array1 = sitk.GetArrayFromImage(image1)
-pixel_array2 = sitk.GetArrayFromImage(image2)
-registered_array = sitk.GetArrayFromImage(registered_image)
-
-# Plot the original and registered images
-plt.figure(figsize=(10, 5))
-plt.subplot(131)
-plt.imshow(pixel_array1, cmap='gray')
-plt.title("Image 1")
-plt.axis('off')
-
-plt.subplot(132)
-plt.imshow(pixel_array2, cmap='gray')
-plt.title("Image 2")
-plt.axis('off')
-
-plt.subplot(133)
-plt.imshow(registered_array, cmap='gray')
-plt.title("Registered Image")
-plt.axis('off')
-
-plt.show()
 
 
 
 
 
 
-
+'''
 # Replace 'image.dcm' with the path to your DICOM file
 image = sitk.ReadImage('mytest.dcm')
 
@@ -184,3 +202,4 @@ def segment_image(image):
 
 #interpolator
 #sitk.sitkLinear
+'''
