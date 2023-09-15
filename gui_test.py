b1d18b416bbbf1b7f26d8e5c171324ccf7d14d93
Kyle Palmer <kylepalmer@Kyles-Air.attlocal.net>
2023-09-15 15:19:05 -0400

    Update gui_test.py

--- gui_test.py
+++ gui_test.py
@@ -1,17 +1,9 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-from deep_learning import DeepLearningModule
-=======
->>>>>>> Stashed changes
-import deep_learning
-import segmentation
-import data
+from tkinter import Canvas, Scrollbar, Frame
 
->>>>>>> 6bd69edcc7bf9384f59c479410ef0d660af5351c
+
 """class AdvancedSegmentationPage:
     def __init__(self, master, core_instance):
         self.master = master
@@ -47,27 +39,52 @@ class ImageScoringPopup:
         self.image1_path = image1_path
         self.image2_path = image2_path
 
+        self.popup_frame = Frame(master)
+        self.popup_frame.pack(fill='both', expand=True)
+
+        self.canvas = Canvas(self.popup_frame)
+        self.canvas.pack(side="left", fill="both", expand=True)
+
+        self.scrollbar = Scrollbar(self.popup_frame, orient="vertical", command=self.canvas.yview)
+        self.scrollbar.pack(side="right", fill="y")
+
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.inner_frame = Frame(self.canvas)
+        self.inner_frame_canvas = self.canvas.create_window((0, 0), window=self.inner_frame, anchor="nw")
+
         self.image1 = tk.PhotoImage(file=image1_path)
         self.image2 = tk.PhotoImage(file=image2_path)
 
-        self.image_label1 = tk.Label(self.master, image=self.image1)
-        self.image_label1.pack(side="left", padx=20, pady=20)
+        self.image_label1 = tk.Label(self.inner_frame, image=self.image1)
+        self.image_label1.pack(pady=(20, 10), anchor="center")
 
-        self.image_label2 = tk.Label(self.master, image=self.image2)
-        self.image_label2.pack(side="right", padx=20, pady=20)
+        self.image_label2 = tk.Label(self.inner_frame, image=self.image2)
+        self.image_label2.pack(pady=(20, 10), anchor="center")
 
-        self.score_label1 = tk.Label(self.master, text="Score Image 1:")
-        self.score_label1.pack(pady=10)
-        self.score_entry1 = tk.Scale(self.master, from_=1, to=10, orient="horizontal", sliderrelief='flat')
-        self.score_entry1.pack()
+        self.score_label1 = tk.Label(self.inner_frame, text="Score Image 1:")
+        self.score_label1.pack(pady=10, anchor="center")
 
-        self.score_label2 = tk.Label(self.master, text="Score Image 2:")
-        self.score_label2.pack(pady=10)
-        self.score_entry2 = tk.Scale(self.master, from_=1, to=10, orient="horizontal", sliderrelief='flat')
-        self.score_entry2.pack()
+        self.score_entry1 = tk.Scale(self.inner_frame, from_=1, to=10, orient="horizontal", sliderrelief='flat')
+        self.score_entry1.pack(pady=10, anchor="center")
 
-        self.submit_button = tk.Button(self.master, text="Submit", command=self.submit_scores)
-        self.submit_button.pack(pady=20)
+        self.score_label2 = tk.Label(self.inner_frame, text="Score Image 2:")
+        self.score_label2.pack(pady=10, anchor="center")
+
+        self.score_entry2 = tk.Scale(self.inner_frame, from_=1, to=10, orient="horizontal", sliderrelief='flat')
+        self.score_entry2.pack(pady=10, anchor="center")
+
+        self.submit_button = tk.Button(self.inner_frame, text="Submit", command=self.submit_scores)
+        self.submit_button.pack(pady=20, anchor="center")
+
+        self.inner_frame.bind("<Configure>", self.on_frame_configure)
+        self.canvas.bind("<Configure>", self.on_canvas_configure)
+
+    def on_frame_configure(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    def on_canvas_configure(self, event):
+        self.canvas.itemconfig(self.inner_frame_canvas, width=event.width)
 
     def submit_scores(self):
         try:
@@ -113,21 +130,18 @@ class Core:
         self.image_scoring_button = tk.Button(self.master, text="Score Images", command=self.open_image_scoring_popup)
         self.image_scoring_button.pack(pady=20)
 
-        self.advanced_segmentation_button = tk.Button(self.master, text="Advanced Segmentation", command=lambda: self.change_buttons([self.deep_learning_button, self.clustering_button, self.advanced_back_button], [self.atlas_segment_button, self.show_image_results_button, self.show_folder_results_button, self.advanced_segmentation_button]))
+        self.advanced_segmentation_button = tk.Button(self.master, text="Advanced Segmentation", command=lambda: self.change_buttons([self.deep_learning_button, self.clustering_button, self.advanced_back_button], [self.advanced_segmentation_button, self.atlas_segment_button, self.show_image_results_button, self.show_folder_results_button,self.clustering_algorithm_label, self.clustering_algorithm_combobox, self.execute_clustering_button]))
         self.advanced_segmentation_button.pack(pady=20)
 
-        self.clustering_button = tk.Button(self.master, text="Clustering", command=lambda:self.change_buttons([self.clustering_back_button],[self.deep_learning_button, self.advanced_back_button, self.clustering_button]))
+        self.clustering_button = tk.Button(self.master, text="Clustering", command=lambda:self.change_buttons([self.clustering_algorithm_label, self.clustering_algorithm_combobox, self.execute_clustering_button, self.clustering_back_button],[self.advanced_segmentation_button, self.deep_learning_button, self.clustering_button, self.advanced_back_button]))
 
-        # Define the Deep Learning button here with the correct command
-        self.deep_learning_button = tk.Button(self.master, text="Deep Learning", command=self.start_deep_learning)
-        self.deep_learning_button.pack(pady=20)
+        self.deep_learning_button = tk.Button(self.master, text="Deep Learning", command=lambda:self.change_buttons([self.deeplearning_back_button],[self.deep_learning_button, self.clustering_button,self.clustering_algorithm_label, self.clustering_algorithm_combobox, self.execute_clustering_button, self.advanced_back_button]))
 
-        self.advanced_back_button = tk.Button(self.master, text="Back", command=lambda:self.change_buttons([self.atlas_segment_button, self.show_image_results_button, self.show_folder_results_button],[self.deep_learning_button, self.clustering_button, self.advanced_back_button]))
+        self.advanced_back_button = tk.Button(self.master, text="Back", command=lambda:self.change_buttons([self.advanced_segmentation_button, self.atlas_segment_button, self.show_image_results_button, self.show_folder_results_button],[self.deep_learning_button, self.clustering_button, self.advanced_back_button]))
 
-        self.clustering_back_button = tk.Button(self.master, text="Back", command=lambda:self.change_buttons([self.deep_learning_button, self.clustering_button, self.advanced_back_button],[self.advanced_segmentation_button, self.clustering_back_button]))
+        self.clustering_back_button = tk.Button(self.master, text="Back", command=lambda:self.change_buttons([self.deep_learning_button, self.clustering_button, self.advanced_back_button],[self.clustering_algorithm_label, self.clustering_algorithm_combobox, self.execute_clustering_button,self.clustering_text, self.results_label, self.clustering_back_button]))
 
-        self.deeplearning_back_button = tk.Button(self.master, text="Back", command=lambda:self.change_buttons([self.deep_learning_button, self.clustering_button, self.advanced_back_button],[self.advanced_segmentation_button,self.deeplearning_back_button]))
-
+        self.deeplearning_back_button = tk.Button(self.master, text="Back", command=lambda:self.change_buttons([self.deep_learning_button, self.clustering_button, self.advanced_back_button],[self.deeplearning_back_button]))
 
         """self.image_file_path = 'mytest.png'
         self.image_button = tk.Button(self.master, text="Display Image", command=self.display_file_png)
@@ -143,7 +157,48 @@ class Core:
 
         #self.advanced_segmentation_button = tk.Button(self.master, text="Advanced Segmentation", command=lambda: self.change_buttons([], [self.atlas_segment_button, self.show_image_results_button, self.show_folder_results_button]))
         #self.advanced_segmentation_button.pack(pady=20)
-    
+
+        self.clustering_algorithm_label = tk.Label(self.master, text="Select Clustering Algorithm:")
+        #self.clustering_algorithm_label.pack(pady=10)
+        self.clustering_algorithm_combobox = ttk.Combobox(self.master, values=["K-Means", "DBSCAN", "Hierarchical", "Other"])
+        #self.clustering_algorithm_combobox.pack()
+
+        # Add a button to execute clustering
+        self.execute_clustering_button = tk.Button(self.master, text="Execute Clustering", command=self.execute_clustering)
+        #self.execute_clustering_button.pack(pady=20)
+        
+    def execute_clustering(self):
+        # Get the selected clustering algorithm
+        selected_algorithm = self.clustering_algorithm_combobox.get()
+
+        clustering_results = ""
+        # Implement clustering logic based on the selected algorithm
+        if selected_algorithm == "K-Means":
+        # Implement K-Means clustering logic here
+            clustering_results = "K-Means clustering results..."
+        elif selected_algorithm == "DBSCAN":
+        # Implement DBSCAN clustering logic here
+            clustering_results = "DBSCAN clustering results..."
+        elif selected_algorithm == "Hierarchical":
+        # Implement Hierarchical clustering logic here
+            clustering_results = "Hierarchical clustering results..."
+        elif selected_algorithm == "Other":
+        # Implement your custom clustering algorithm logic here
+            clustering_results = "Other clustering results..."
+
+    # Display clustering results within the GUI or perform any desired actions
+    # Display clustering results within the GUI
+        self.display_clustering_results(clustering_results)
+    # You can use labels or other widgets to display the clustering results.
+    def display_clustering_results(self, clustering_results):
+        # Create a label or canvas to display the clustering results
+        self.results_label = tk.Label(self.master, text="Clustering Results:")
+        self.results_label.pack()
+
+        # Create a text widget to show the clustering results
+        self.clustering_text = tk.Text(self.master, height=10, width=50)
+        self.clustering_text.insert(tk.END, clustering_results)
+        self.clustering_text.pack()
         
     def show_main_window(self):
         self.master.deiconify()  # Show the main window
@@ -154,7 +209,6 @@ class Core:
             print("Selected folder:", folder_path)
             self.selected_folder = folder_path
             self.update_folder_label()
-
 
     def update_folder_label(self):
         self.folder_label.config(text="Selected Folder: " + self.selected_folder)
@@ -184,27 +238,15 @@ class Core:
         # image2_path = "C:\\Users\\kevin\\Documents\\classes\\ED1\\BrainAI-Segmentation\\scan 1\\ADNI_003_S_1257_PT_ADNI_br_raw_20070510122011437_2_S32031_I54071.png"
         image1_path = "/Users/kylepalmer/Documents/GitHub/BrainAI-Segmentation/scan 1/ADNI_003_S_1257_PT_ADNI_br_raw_20070510122011156_1_S32031_I54071.png"  # Replace with actual image paths
         image2_path = "/Users/kylepalmer/Documents/GitHub/BrainAI-Segmentation/scan 1/ADNI_003_S_1257_PT_ADNI_br_raw_20070510122011437_2_S32031_I54071.png"
-        
+
         popup_window = tk.Toplevel(self.master)
         image_scoring_popup = ImageScoringPopup(popup_window, image1_path, image2_path, self.save_scores)
+        
 
     def save_scores(self, score1, score2):
         # Implement your logic to save the scores here
         print("Score for Image 1:", score1)
         print("Score for Image 2:", score2)
-        # update the global vars in the deep learning module
-        deep_learning.get_user_score(score1, score2)
-
-    def start_deep_learning(self):
-        # Implement your deep learning logic here
-        print("Initiating Deep Learning tasks")  
-        region_data = {
-            "Region1": "scan2/ADNI_003_S_1059_PT_adni2__br_raw_20071211125948781_11_S43552_I84553.dcm",
-            "Region2": "scan2/ADNI_003_S_1059_PT_adni2__br_raw_20071211125949312_18_S43552_I84553.dcm",
-            # Add more regions as needed
-        }  
-                # Call the load_regions function from the Deep Learning module
-        deep_learning.load_regions(region_data)
     
 
     """def show_clustering_buttons(self):
@@ -224,20 +266,10 @@ class Core:
         print("Displaying segmentation results for images in a folder") 
 
     def change_buttons(self, show_list, hide_list):
-        for button in show_list:
-            button.pack(pady=20)
         for button in hide_list:
-            button.pack_forget()   
-    
-    # a function that will take a dictionary with regions (keys) and coordinates (values) as input,
-    # it will call create_seg_img() from the segmenation module; it's gonna spit out images 
-    # then we're calling the function in data that's saving the segmented image on file
-    # this is not yet complete, needs testing
-    def save_segmentation(image, region_dict, new_dir):
-        region_images = segmentation.create_seg_images(image, region_dict)
-        data.store_seg_img_on_file(region_images, new_dir)
-        print("save_segmentation() has been called")
-
+            button.pack_forget()
+        for button in show_list:
+            button.pack(pady=20) 
 
 """class ClusteringPage:
     def __init__(self, master, core_instance):
@@ -278,7 +310,6 @@ class DeepLearningPage:
     def go_back(self):
         self.hide_buttons()
         self.core_instance.advanced_segmentation_page.show_buttons()"""
-
 
 # Usage
 if __name__ == "__main__":
