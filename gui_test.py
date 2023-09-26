a596d0a3b9ca42ddc4c661af6a9f6b16e40edc55
dustinsherwood <102439006+dustinsherwood@users.noreply.github.com>
2023-09-26 18:13:28 -0400

    segment results example

--- gui_test.py
+++ gui_test.py
@@ -382,8 +382,8 @@ class Core:
 
     def open_image_scoring_popup(self):
         image_paths = [
-        "scan1_pngs\ADNI_003_S_1257_PT_ADNI_br_raw_20070510122011156_1_S32031_I54071.png",  # Replace with actual image paths
-        "scan1_pngs\ADNI_003_S_1257_PT_ADNI_br_raw_20070510122011437_2_S32031_I54071.png"]
+        "scan1_pngs/ADNI_003_S_1257_PT_ADNI_br_raw_20070510122011156_1_S32031_I54071.png",  # Replace with actual image paths
+        "scan1_pngs/ADNI_003_S_1257_PT_ADNI_br_raw_20070510122011437_2_S32031_I54071.png"]
 
         popup_window = tk.Toplevel(self.master)
         image_scoring_popup = ImageScoringPopup(popup_window, image_paths, self.save_scores)
