ab79a28db03f31af5d364d5e1dc136952e03744c
dustinsherwood <102439006+dustinsherwood@users.noreply.github.com>
2023-11-03 18:42:26 -0400

    updated DL
    old

--- deep_learning.py
+++ deep_learning.py
@@ -1,255 +1,193 @@
 import SimpleITK as sitk
-import os
 import tensorflow as tf
 import numpy as np
 from scipy.ndimage import convolve
 import data
+from tensorflow.keras.models import load_model
+import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
-#dummy input data
-sitk_images_dict = {
-    "image1": data.get_3d_image("scan1"), # gets 3d sitk image from a folder of DCM images
-    "image2": data.get_3d_image("scan2"),   
-    # Add other images...
-}
 
-#for the dummyDL functiom
-numpyImagesDict = {key: sitk.GetArrayFromImage(img) for key, img in sitk_images_dict.items()}
+# Function to split 3D images into smaller sub-arrays
+def split_into_subarrays(img_array, depth=5):
+    total_slices = img_array.shape[0]
+    sub_arrays = [img_array[i:i+depth, :, :] for i in range(0, total_slices, depth) if i+depth <= total_slices]
+    return sub_arrays
 
-#for the dummyDL function
-#labeled data is tricky, because it should probably be in the same format as the input data + a 1 or 0 label
-#dictionary makes sense, but the input data is a 3x3x3 (unless we pass a window size other than default) np array,
-# or rather, a stack of 3x3x3 arrays inside another array, because that's more optimized
-dummyLabels = {
-    "image1": 1,
-    "image2": 0
-    # Add more labels...
-}
+def weighted_binary_crossentropy(y_true, y_pred):
+    # custom weights for binary cross entropy
+    weight_0 = 1.0  # for regions
+    weight_1 = 2.0  # for boundaries
+    b_ce = tf.keras.backend.binary_crossentropy(y_true, y_pred)
+    weight_vector = y_true * weight_1 + (1. - y_true) * weight_0
+    weighted_b_ce = weight_vector * b_ce
+    return tf.keras.backend.mean(weighted_b_ce)
 
-#normalizes pixel value of 3d array dict
-def normalize_np_dict(volume3dDict):
+
+# Function to create a U-Net model for 3D image segmentation
+def unet(input_size=(5, 128, 128, 1)):  # Notice the change in the last dimension
+    inputs = tf.keras.layers.Input(input_size)
+    
+# Encoder layers (convolutions and pooling)
+    conv1 = tf.keras.layers.Conv3D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = tf.keras.layers.Conv3D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    pool1 = tf.keras.layers.MaxPooling3D(pool_size=(1, 2, 2))(conv1)
+    
+    conv2 = tf.keras.layers.Conv3D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv2 = tf.keras.layers.Conv3D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
+    pool2 = tf.keras.layers.MaxPooling3D(pool_size=(1, 2, 2))(conv2)
+    
+    conv3 = tf.keras.layers.Conv3D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
+    conv3 = tf.keras.layers.Conv3D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
+    pool3 = tf.keras.layers.MaxPooling3D(pool_size=(1, 2, 2))(conv3)
+    
+    conv4 = tf.keras.layers.Conv3D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
+    conv4 = tf.keras.layers.Conv3D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
+    drop4 = tf.keras.layers.Dropout(0.3)(conv4)
+    pool4 = tf.keras.layers.MaxPooling3D(pool_size=(1, 2, 2))(drop4)
+    
+    conv5 = tf.keras.layers.Conv3D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
+    conv5 = tf.keras.layers.Conv3D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+    drop5 = tf.keras.layers.Dropout(0.3)(conv5)
+
+    up6 = tf.keras.layers.UpSampling3D(size=(1, 2, 2))(drop5)
+    merge6 = tf.keras.layers.concatenate([drop4, up6], axis=-1)
+    conv6 = tf.keras.layers.Conv3D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+
+    up7 = tf.keras.layers.UpSampling3D(size=(1, 2, 2))(conv6)
+    merge7 = tf.keras.layers.concatenate([conv3, up7], axis=-1)
+    conv7 = tf.keras.layers.Conv3D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
+
+    up8 = tf.keras.layers.UpSampling3D(size=(1, 2, 2))(conv7)
+    merge8 = tf.keras.layers.concatenate([conv2, up8], axis=-1)
+    conv8 = tf.keras.layers.Conv3D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
+
+    up9 = tf.keras.layers.UpSampling3D(size=(1, 2, 2))(conv8)
+    merge9 = tf.keras.layers.concatenate([conv1, up9], axis=-1)
+    conv9 = tf.keras.layers.Conv3D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
+    
+    # Define output layer
+    outputs = tf.keras.layers.Conv3D(1, (1, 1, 1), activation='sigmoid')(conv9)
+    
+    # Compile and return the model
+    model = tf.keras.models.Model(inputs=[inputs], outputs=[outputs])
+    model.compile(optimizer='adam', loss=weighted_binary_crossentropy, metrics=['accuracy'])
+    return model
+
+
+
+def get_surrounding_slices(original_slice, sub_arrays, slice_index, depth):
+    surrounding_depth = depth // 2
+    surrounding_slices = []
+    for sub_array in sub_arrays:
+        for idx, slice_ in enumerate(sub_array):
+            if np.array_equal(slice_, original_slice):
+                start_idx = max(0, idx - surrounding_depth)
+                end_idx = min(len(sub_array), idx + surrounding_depth + 1)
+                surrounding_slices = sub_array[start_idx:end_idx]
+    if len(surrounding_slices) != depth:
+        # Handle edge cases by padding with zeros
+        padding_slices = depth - len(surrounding_slices)
+        pad_before = padding_slices // 2
+        pad_after = padding_slices - pad_before
+        surrounding_slices = np.pad(surrounding_slices, ((pad_before, pad_after), (0, 0), (0, 0)), 'constant')
+    return surrounding_slices
+
+
+def normalizeTF(volume3dDict):
     normalizedDict = {}
     for key, value in volume3dDict.items():
         tensor = tf.convert_to_tensor(value, dtype=tf.float32)
         minVal = tf.reduce_min(tensor)
         maxVal = tf.reduce_max(tensor)
         normalizedTensor = (tensor - minVal) / (maxVal - minVal)
-        
-        # Convert back to numpy and store it in the dictionary
         normalizedDict[key] = normalizedTensor.numpy()
-
-        #note: it would be more optimized to normalize each input window rather than 
-        # normalizing the entire image. Prob doesn't matter
     return normalizedDict
 
-
-#pix by pix classifier, not built with user score in mind
-def buildPixelModel(window_size=3):
-    # Assumes input is a 3D patch of size [window_size, window_size, window_size]
-    model = tf.keras.Sequential([
-        tf.keras.layers.InputLayer(input_shape=(window_size, window_size, window_size, 1)),
-        tf.keras.layers.Conv3D(32, (window_size, window_size, window_size), activation='relu', padding='valid'),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(2, activation='softmax')  # Assumes binary classification
-    ])
-
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    return model          
-
-
-
-#finds edges of the image, only need to classify edges, not the entire thing
 def find_boundary(segment):
-    # Define a kernel for 3D convolution that checks for 26 neighbors in 3D
+    segment_copy = segment.copy()  # Work on a copy to avoid modifying the original
     kernel = np.ones((3, 3, 3))
     kernel[1, 1, 1] = 0
-    
-    # Convolve with the segment to find the boundary of ROI
-    boundary = convolve(segment > 0, kernel) > 0
-    
-    return boundary
+    boundary = convolve(segment_copy > 0, kernel) > 0
+    boundary = boundary & (segment_copy > 0)
+    segment_copy[boundary] = 1  # Label the boundary as 1
+    segment_copy[~boundary] = 0  # Label the rest as 0
+    return segment_copy
 
-#takes boundary (edges), and gets 3d windows around each boundary voxel. These are inputs to the model
-def extract_windows(volume, window_size=3):
-    boundary = find_boundary(volume)
-    padding = window_size // 2
-    padded_volume = np.pad(volume, ((padding, padding), (padding, padding), (padding, padding)), mode='constant')
-    windows = []
-    indices = []
-    
-    boundary_indices = np.argwhere(boundary)  # Find the indices of boundary voxels
-    
-    for index in boundary_indices:
-        z, y, x = index
-        window = padded_volume[z:z + window_size, y:y + window_size, x:x + window_size]
-        windows.append(window)
-        indices.append((z, y, x))
-    
-    #does it make sense to convert to arrays?
-    return np.array(windows), np.array(indices)
+def show_slices(triplets):
+    n = len(triplets)
+    fig, axes = plt.subplots(1, n * 2, figsize=(12, 6))
+    for i in range(n):
+        orig, pred = triplets[i]
+        axes[i * 2].imshow(orig.T, cmap="gray", origin="lower")
+        pred_2d = np.squeeze(pred.T)
+        axes[i * 2 + 1].imshow(pred_2d, cmap="gray", origin="lower")
+    plt.suptitle("Original and Segmented")
+    plt.show()
 
-
-def train_model(model, windows, user_score, labels=None):
-    model.fit(windows, labels, epochs= 20 - int(user_score * 10)) 
-
-# not tested yet -Kevin
-def executeDL(dict_of_np_arrays, user_score=0, model=buildPixelModel()):
+def dlAlgorithm(segmentDict, depth=5):
+    numpyImagesDict = {key: sitk.GetArrayFromImage(img) for key, img in segmentDict.items()}
+    normalizedDict = normalizeTF(numpyImagesDict)
     
-    #should only have to normalize data once, and we only need to pass dict_of_np_arrays once
-    normalized_data = normalize_np_dict(dict_of_np_arrays) 
+    # Define the U-Net model
+    model = unet(input_size=(depth, 128, 128, 1))
 
-    #should only have to compile once?
-    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics = ["accuracy"])
-    
-    #train model on on dict of np arrays,
-        # Dummy labels for demonstration; replace with actual labels if available
-    #need to run for each key/value?
-    classification_dict = {}
-    for region, seg_volume in normalized_data.items():
-        windows, indices = extract_windows(seg_volume)
-        windows = windows[..., np.newaxis]  # Adding a channel dimension
+    #early_stopping = EarlyStopping(patience=5, verbose=1)
+    #model_checkpoint = ModelCheckpoint("best_model.keras", save_best_only=True, verbose=1)
+    #callbacks_list = [early_stopping, model_checkpoint]
+
+    loss_list = []
+    all_triplets = []  # <-- Store all the segmented triplets here
+
+    # First loop for segmentation
+    for sub_array in normalizedDict.items():
+        sub_arrays_split = split_into_subarrays(sub_array, depth)
         
-        train_model(model, windows, dummyLabels, user_score)
-        
-        predictions = model.predict(windows)
-        predicted_labels = np.argmax(predictions, axis=1)
-    
-        classified_indices = indices[predicted_labels == 1]
-        classification_dict[region] = classified_indices
-
-    #return segmentation attempts (dictionary of coordinates)
-    #return model
-    return normalized_data, model, classification_dict
-
-    #segmentation attemps displayed by core, user score collected
-    #this function called again, passing the model back, and passing in user score
-    #dummyDL is run on a loop, probably in core?
-
-#class version of executeDL: no need to output and reinput things other than user_score
-#Notes: labeled data could be a bunch of 'windows', labeled 0 or 1
-#   should windows also be a class attribute? why extract windows many times
-#   if windows are a class attribute, than we wouldn't need to keep the entire image in memory
-class CustomClassifier:
-    def __init__(self, initial_model=None, labeled_data=None):
-        self.model = initial_model if initial_model else buildPixelModel()
-        self.classification_dict = {}
-        self.normalized_data = None
-        self.labeled_data = labeled_data
-        self.windows = None
-
-    def executeDL(self, dict_of_np_arrays=None, user_score=0):
-        if self.normalized_data == None:
-            self.normalized_data = normalize_np_dict(dict_of_np_arrays)
-
-        # No need to compile multiple times, so we check if it's compiled.
-        if not hasattr(self.model, 'optimizer'):
-            self.model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-
-        for region, seg_volume in self.normalized_data.items():
-            if self.windows == None:
-                windows, indices = extract_windows(seg_volume)
-                self.windows = windows[..., np.newaxis]  # Adding a channel dimension
-
-            # Using the labeled_data for the specific region if available
-            region_labels = self.labeled_data.get(region) if self.labeled_data else None
-            #labeled data needs to be redone, to fit with the input data expected
-
-            train_model(self.model, windows, user_score, region_labels)
+        for idx, sub_arr in enumerate(sub_arrays_split):
+            surrounding_slices = get_surrounding_slices(sub_arr[depth//2], sub_arrays_split, idx, depth)
+            model = load_model("current_model.keras", custom_objects={"weighted_binary_crossentropy": weighted_binary_crossentropy})
             
-            #make predictions
-            predictions = self.model.predict(windows)
+            sub_boundary_array = find_boundary(surrounding_slices)
+            sub_arr_exp = np.expand_dims(np.expand_dims(surrounding_slices, axis=0), axis=-1)
+            sub_boundary_array_exp = np.expand_dims(np.expand_dims(sub_boundary_array, axis=0), axis=-1)
 
-            #average predicted probability for positive class
-            avg_prediction = np.mean(predictions[:, 1])
+            history = model.train_on_batch(sub_arr_exp, sub_boundary_array_exp)
+            loss_list.append(history[0])
 
-            #what datastruct is predictions? does this round or just make things 1? Find the highest preds?
-            predicted_labels = np.argmax(predictions, axis=1)
+            pred = model.predict(sub_arr_exp)
+            middle_index = depth // 2
+            slices_triplet = (surrounding_slices[middle_index], pred[0][middle_index])
+            all_triplets.append(slices_triplet)
 
-            #Define a loss based on the user's score
-            # We can use mean squared error here, but other choices might be appropriate depending on the problem
-            loss = (avg_prediction - user_score) ** 2
+    # Second loop for displaying the segmented images in batches of 3
+    for i in range(0, len(all_triplets), 3):
+        batch_triplets = all_triplets[i:i+3]
 
-            # Update the model
-            # This part is tricky without labeled data; we need a way to compute gradients
-            # One option is to use a library like TensorFlow's 'tape' mechanism
-            with tf.GradientTape() as tape:
-                tape.watch(self.model.trainable_variables)
-                # Repredict to get the model's outputs as TensorFlow tensors
-                predictions_tf = self.model(windows, training=True)
-                avg_prediction_tf = tf.reduce_mean(predictions_tf[:, 1])
-                loss_tf = (avg_prediction_tf - user_score) ** 2
-                grads = tape.gradient(loss_tf, self.model.trainable_variables)
-                optimizer = tf.keras.optimizers.Adam()
-                optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+        for idx, triplet in enumerate(batch_triplets):
+            print(f"Triplet {idx + 1} Original Min: {triplet[0].min()}, Max: {triplet[0].max()}")
+            print(f"Triplet {idx + 1} Prediction Min: {triplet[1].min()}, Max: {triplet[1].max()}")
 
-            classified_indices = indices[predicted_labels == 1]
-            self.classification_dict[region] = classified_indices
+        show_slices(batch_triplets)
 
-        return self.classification_dict
+        proceed = input("Would you like to see more slices? (y/n): ")
+        if proceed.lower() != 'y':
+            break  # Exit the function entirely
+
+    model = load_model("best_model.keras", custom_objects={"weighted_binary_crossentropy": weighted_binary_crossentropy})
     
+    plt.plot(loss_list)
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Batch')
+    plt.show()
 
 
-if __name__ == '__main__':
-   print("running dl module")
-   classifier = CustomClassifier()
-   
-
-'''
-#class not needed
-class DeepLearningModule:
-    def __init__(self):
-        self.atlas_segmentation_data = {}
-        self.user_score1 = -1
-        self.user_score2 = -2
-
-    def load_regions(self, region_data):
-        for region_name, sitk_name in region_data.items():
-            try:
-                region_image = sitk.ReadImage(sitk_name)
-                print(f"Loaded {region_name} from {sitk_name}")
-            except Exception as e:
-                print(f"Error loading {region_name} from {sitk_name}: {e}")
-
-    def load_atlas_data(self, atlas_data1, atlas_data2):
-        for folder_path in [atlas_data1, atlas_data2]:
-            for filename in os.listdir(folder_path):
-                file_path = os.path.join(folder_path, filename)
-                try:
-                    atlas_image = sitk.ReadImage(file_path)
-                    self.atlas_segmentation_data[filename] = atlas_image
-                    print(f"Loaded atlas data from {file_path}")
-                except Exception as e:
-                    print(f"Error loading atlas data from {file_path}: {e}")
 
 
-# Existing user score global variables and function
-#will prob be removed, user score will be supplied to dl algo as argument from core
-user_score1 = -1
-user_score2 = -2
+if __name__ == "__main__":
+    sitk_images_dict = {
+        "image1": data.get_3d_image("scan1"),
+        "image2": data.get_3d_image("scan2"),
+    }
 
-#will be removed, user score updated in core
-def get_user_score(x1, x2):
-    global user_score1, user_score2
-    user_score1 = x1
-    user_score2 = x2
-    print("score 1 is: ", user_score1)
-    print("score 2 is: ", user_score2)
-'''
-
-
-'''
-#had two models that essentially did the same thing, 
-#idk what the difference between them is. 
-#moved the second here to avoid confusion
-def build_boundary_window_model(window_size=3):
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv3D(16, (3, 3, 3), activation='relu', input_shape=(window_size, window_size, window_size, 1)),
-        tf.keras.layers.MaxPooling3D(),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(32, activation='relu'),
-        tf.keras.layers.Dense(2, activation='softmax')  # Assuming binary classification (0 or 1)
-    ])
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    return model
-'''
-
+    #dlAlgorithm(sitk_images_dict)
