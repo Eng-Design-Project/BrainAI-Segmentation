fixed the overflow error we were getting b4

data.py
@@ -641,23 +641,29 @@ def avg_brightness(img_dict, coords_dict):
                     # Normalize each image to the range [0, 255]
                     # pixel_value = ((pixel_value - min) / (max - min)) * 255
                     # Calculate the average pixel brightness for the normalized image
+                    if z > 44:
+                        print(pixel_value)
                     avg_brightness += pixel_value
 
+            print('AVERAGE BRIGHTNESS')
+            print(avg_brightness)
             avg_brightness = avg_brightness / count
-        brightness_dict[region] = avg_brightness
+        brightness_dict[region] = avg_brightness * 255
 
     return brightness_dict
 
 def min_max_normalize(arr):
-    min_val = np.min(arr)
-    max_val = np.max(arr)
+    arr64 = arr.astype(np.float64)
+
+    min_val = np.min(arr64)
+    max_val = np.max(arr64)
 
     # Check if max and min values are the same (to avoid division by zero)
     if max_val - min_val == 0:
-        return arr
+        return arr64
 
-    normalized_arr = (arr - min_val) / (max_val - min_val)
-    return normalized_arr
+    normalized_arr64 = (arr64 - min_val) / (max_val - min_val)
+    return normalized_arr64
 
 
 def is_segment_results_dir(directory):