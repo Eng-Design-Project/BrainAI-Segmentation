d5ef5d39ccc54297e78a9f06fb0a1c879df1edbd
Kyle Palmer <kylepalmer@Kyles-Air.attlocal.net>
2023-12-05 11:09:06 -0500

    Update Unet_Segmentation.py

--- Unet_Segmentation.py
+++ Unet_Segmentation.py
@@ -1,53 +1,36 @@
 import tensorflow as tf
 import numpy as np
 from scipy.ndimage import convolve
-from skimage.transform import resize
 import data
 import matplotlib.pyplot as plt
 import os
 from keras.models import load_model
-from tensorflow.keras.utils import to_categorical
-import glob
-import pydicom
-import tkinter as tk
-from tkinter import simpledialog
 
-
-def ensure_directory_exists(path):
-    directory = os.path.dirname(path)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-# Function to split 3D images into smaller sub-arrays
+# Function to split a 3D image into smaller sub-arrays of a specified depth.
+# This is useful for processing large 3D images in smaller chunks.
 def split_into_subarrays(img_array, depth=5):
     total_slices = img_array.shape[0]
     sub_arrays = [img_array[i:i+depth, :, :] for i in range(0, total_slices, depth) if i+depth <= total_slices]
     return sub_arrays
 
-# def weighted_binary_crossentropy(y_true, y_pred):
-#     # custom weights for binary cross entropy
-#     weight_0 = 1.0  # for regions
-#     weight_1 = 2.0  # for boundaries
-#     b_ce = tf.keras.backend.binary_crossentropy(y_true, y_pred)
-#     weight_vector = y_true * weight_1 + (1. - y_true) * weight_0
-#     weighted_b_ce = weight_vector * b_ce
-#     return tf.keras.backend.mean(weighted_b_ce)
-
+# Custom loss function for binary cross-entropy with optional weighting.
+# This can be used to give different importance to different classes during training.
 def weighted_binary_crossentropy(y_true, y_pred):
-    # Compute the binary crossentropy
     b_ce = tf.keras.backend.binary_crossentropy(y_true, y_pred)
-
-    # If you have weights to apply, modify the cross-entropy here
-    # Example: weighted_b_ce = apply_weights_to_b_ce(b_ce, weights)
-
+    # Modify this function if you need to apply weights to the binary cross-entropy.
     return b_ce
 
 
 # Function to create a U-Net model for 3D image segmentation
+# U-Net is a convolutional network architecture for fast and precise segmentation of both whole brains and segments.
 def unet_generate_model(input_size=(5, 128, 128, 1)): 
     inputs = tf.keras.layers.Input(input_size)
     
-    # Encoder layers (convolutions and pooling)
+    # Encoder part: series of convolutions and pooling layers to capture features.
+    # Each block contains Convolution -> Activation -> Convolution -> MaxPooling.
+    # The number of filters doubles with each block.
+    # Decoder part: series of upsampling and concatenation with corresponding encoder outputs.
+    # Each block contains UpSampling -> Concatenation -> Convolution -> Activation.
     conv1 = tf.keras.layers.Conv3D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
     conv1 = tf.keras.layers.Conv3D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
     pool1 = tf.keras.layers.MaxPooling3D(pool_size=(1, 2, 2))(conv1)
@@ -86,67 +69,28 @@ def unet_generate_model(input_size=(5, 128, 128, 1)):
     conv9 = tf.keras.layers.Conv3D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
     
     # Define output layer
+    # Final layer: Convolution to get to the desired number of output channels.
     outputs = tf.keras.layers.Conv3D(1, (1, 1, 1), activation='sigmoid')(conv9)
     
     # Compile and return the model
+    # Creating the model and compiling it with the defined optimizer, loss function, and metrics.
     model = tf.keras.models.Model(inputs=[inputs], outputs=[outputs])
     model.compile(optimizer='adam', loss=weighted_binary_crossentropy, metrics=['accuracy'])
     return model
 
-
-# def unet_internal(input_size=(128, 128, 1), num_classes=5):
-#     inputs = tf.keras.layers.Input(input_size)
-
-#     # Encoder layers (convolutions and pooling)
-#     conv1 = tf.keras.layers.Conv2D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-#     conv1 = tf.keras.layers.Conv2D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
-#     pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv1)
-    
-#     conv2 = tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
-#     conv2 = tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv2)
-#     pool2 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv2)
-    
-#     conv3 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool2)
-#     conv3 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv3)
-#     pool3 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(conv3)
-    
-#     conv4 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool3)
-#     conv4 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv4)
-#     drop4 = tf.keras.layers.Dropout(0.3)(conv4)
-#     pool4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(drop4)
-    
-#     conv5 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool4)
-#     conv5 = tf.keras.layers.Conv2D(256, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
-#     drop5 = tf.keras.layers.Dropout(0.3)(conv5)
-
-#     # Decoder layers (up-sampling and concatenation)
-#     up6 = tf.keras.layers.UpSampling2D(size=(2, 2))(drop5)
-#     merge6 = tf.keras.layers.concatenate([drop4, up6], axis=-1)
-#     conv6 = tf.keras.layers.Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-
-#     up7 = tf.keras.layers.UpSampling2D(size=(2, 2))(conv6)
-#     merge7 = tf.keras.layers.concatenate([conv3, up7], axis=-1)
-#     conv7 = tf.keras.layers.Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge7)
-
-#     up8 = tf.keras.layers.UpSampling2D(size=(2, 2))(conv7)
-#     merge8 = tf.keras.layers.concatenate([conv2, up8], axis=-1)
-#     conv8 = tf.keras.layers.Conv2D(32, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge8)
-
-#     up9 = tf.keras.layers.UpSampling2D(size=(2, 2))(conv8)
-#     merge9 = tf.keras.layers.concatenate([conv1, up9], axis=-1)
-#     conv9 = tf.keras.layers.Conv2D(16, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge9)
-
-#     # Output layer
-#     outputs = tf.keras.layers.Conv2D(num_classes, (1, 1), activation='softmax')(conv9)
-
-#     # Compile the model
-#     model = tf.keras.models.Model(inputs=[inputs], outputs=[outputs])
-#     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-#     return model
-
+# Function to generate predictions for each subarray using the provided model.
+# Useful for processing each chunk of the large 3D image separately.
 def generate_predictions(subarrays, model):
     predictions = []
+    correct_shape_count = sum(1 for sub_arr in subarrays if sub_arr.shape == (5, 128, 128))
+
+    if correct_shape_count == 0:
+        print("No subarrays with the correct shape.")
+    if not subarrays:
+        print("Input subarrays list is empty.")
+
+    # Iterate over each subarray, reshape it to the model's expected input shape,
+    # perform the prediction, and store the result.
     for sub_arr in subarrays:
         # Check if the subarray has the correct shape for the model
         if sub_arr.shape == (5, 128, 128):
@@ -164,8 +108,13 @@ def generate_predictions(subarrays, model):
             print("Subarray with incorrect shape encountered:", sub_arr.shape)
             continue
 
-    # Concatenate all predictions along the depth axis
-    combined_prediction = np.concatenate(predictions, axis=0)
+    if len(predictions) > 0:
+        # Concatenate all predictions along the depth axis
+        combined_prediction = np.concatenate(predictions, axis=0)
+    
+    else:
+        print("predictions is empty")
+        combined_prediction = np.array([])
 
     print("generate predictions complete")
     print(combined_prediction.shape)
@@ -181,7 +130,7 @@ def get_surrounding_slices(original_slice, sub_arrays, depth):
                 start_idx = max(0, idx - surrounding_depth)
                 end_idx = min(len(sub_array), idx + surrounding_depth + 1)
                 surrounding_slices = sub_array[start_idx:end_idx]
-                break  # You can break here since you've found the slice and don't need to check further
+                break  
     if len(surrounding_slices) != depth:
         # Handle edge cases by padding with zeros
         padding_slices = depth - len(surrounding_slices)
@@ -190,11 +139,12 @@ def get_surrounding_slices(original_slice, sub_arrays, depth):
         surrounding_slices = np.pad(surrounding_slices, ((pad_before, pad_after), (0, 0), (0, 0)), 'constant')
     return surrounding_slices
 
-
-
-    
+# Function to normalize 3D volumes.
+# Normalization is crucial for consistent model training and prediction.
 def normalizeTF(volume3dDict):
     normalizedDict = {}
+    # For each 3D volume, convert it to a TensorFlow tensor, find min and max,
+    # and normalize the volume to a 0-1 range.
     for key, value in volume3dDict.items():
         tensor = tf.convert_to_tensor(value, dtype=tf.float32)
         minVal = tf.reduce_min(tensor)
@@ -203,6 +153,8 @@ def normalizeTF(volume3dDict):
         normalizedDict[key] = normalizedTensor.numpy()
     return normalizedDict
 
+# Function to find the boundary of a segmented region.
+# Used in post-processing to delineate the edges of segmented regions.
 def find_boundary(segment):
     # Check if the segment is 2D or 3D and choose the kernel accordingly
     if segment.ndim == 3:  # 3D data
@@ -219,6 +171,7 @@ def find_boundary(segment):
     segment_copy[boundary] = 1  # Label the boundary as 1
     segment_copy[~boundary] = 0  # Label the rest as 0
     return segment_copy
+
 
 '''def show_slices(triplets): This returns the images in color. This was an accident but they looked cool so I just commented it out insteaad of deleting it
     n = len(triplets)
@@ -237,62 +190,19 @@ def find_boundary(segment):
     plt.show()
 '''
 
-def show_slices(triplets, threshold=0.5, brightening_factor=1.3):  # Adjust threshold and brightening factor as needed
-    n = len(triplets)
-    fig, axes = plt.subplots(2, n, figsize=(2*n, 4))
-
-    for i, (orig, pred) in enumerate(triplets):
-        orig = np.squeeze(orig).astype(np.float32)  # Ensure original is float32 to prevent clipping
-        if orig.ndim != 2:
-            raise ValueError(f"Original image has unexpected dimensions: {orig.shape}")
-
-        # Prediction processing
-        if pred.ndim == 3 and pred.shape[-1] > 1:
-            pred = np.argmax(pred, axis=-1)
-        elif pred.ndim == 3 and pred.shape[-1] == 1:
-            pred = np.squeeze(pred)
-        elif pred.ndim != 2:
-            raise ValueError(f"Prediction has unexpected dimensions: {pred.shape}")
-
-        binary_mask = pred > threshold
-
-        # Apply brightening
-        brightened_image = np.copy(orig)
-        brightened_image[binary_mask] *= brightening_factor
-        brightened_image = np.clip(brightened_image, 0, np.max(orig))  # Clip to the max of original to prevent overexposure
-
-        axes[0, i].imshow(orig, cmap="gray")
-        axes[0, i].set_title("Original")
-        axes[0, i].axis('off')
-
-        axes[1, i].imshow(brightened_image, cmap="gray")  # Show the brightened image
-        axes[1, i].set_title("Segmented")
-        axes[1, i].axis('off')
-
-    plt.show()
-
-
-
-def get_unet_result_coordinates(predict_3d, threshold=0.5):
-    coordinates_dict = {}
+# Function to generate coordinates below a certain threshold from the 3D prediction.
+def get_unet_result_coordinates(predict_3d, threshold=0.7):
+    coordinates_list = []
     # Get coordinates below the threshold
     for x in range(predict_3d.shape[0]):
         for y in range(predict_3d.shape[1]):
             for z in range(predict_3d.shape[2]):
-                if predict_3d[x, y, z, 0] < threshold:  # Assuming the last dimension is the channel
-                    coordinates_dict.setdefault('coordinates', []).append([x, y, z])
-    return coordinates_dict
+                if predict_3d[x, y, z] < threshold:  # Assuming the last dimension is the channel
+                    coordinates_list.append([x, y, z])
+    return coordinates_list
 
 
-def normalize_image(image):
-    min_val = np.min(image)
-    max_val = np.max(image)
-    if max_val - min_val > 0:
-        normalized_image = (image - min_val) / (max_val - min_val)
-    else:
-        normalized_image = image - min_val
-    return normalized_image
-
+# Processes the subarrays to create training samples and their corresponding labels.
 def prepare_data_for_training(subarrays, depth=5):
     X_train = []
     Y_train = []
@@ -319,20 +229,9 @@ def prepare_data_for_training(subarrays, depth=5):
 
     return X_train, Y_train
 
-
-
-def get_user_selection(region_options):
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
-
-    region_selection = simpledialog.askinteger("Select Region",
-                                               "Select the region to visualize:\n" +
-                                               "\n".join([f"{k}: {v}" for k, v in region_options.items()]),
-                                               parent=root)
-    return region_selection
-
-
-def execute_unet(inputDict, depth=5, threshold=0.5):
+# Main function to execute the U-Net process on the input data.
+# Orchestrates the whole process of loading data, training/predicting with U-Net, and processing results.
+def execute_unet(inputDict, depth=5):
     dict_of_3d_arrays = {}
     new_dict = {}
 
@@ -351,11 +250,14 @@ def execute_unet(inputDict, depth=5, threshold=0.5):
     for key, array3d in normalizedDict.items():
         if os.path.exists(model_paths[key]):
             # Path exists
+            print(array3d.shape)
+            data.display_3d_array_slices(array3d, 5)
             print(f"The path for '{key}' exists.")
             subarrays_split = split_into_subarrays(array3d)
             model_binary = load_model(model_paths[key], custom_objects={"weighted_binary_crossentropy": weighted_binary_crossentropy})
             predict_3d = generate_predictions(subarrays_split, model_binary)
-            coordinates_below_threshold = get_unet_result_coordinates(predict_3d, threshold)
+            
+            coordinates_below_threshold = get_unet_result_coordinates(predict_3d)
             final_output[key] = coordinates_below_threshold
         else:
             # Path does not exist
@@ -368,33 +270,20 @@ def execute_unet(inputDict, depth=5, threshold=0.5):
             model.fit(X_train, Y_train, epochs=10, batch_size=16)
             model.save(model_paths[key])
             predict_3d = generate_predictions(subarrays, model)
-            coordinates_below_threshold = get_unet_result_coordinates(predict_3d, threshold)
+            coordinates_below_threshold = get_unet_result_coordinates(predict_3d)
             final_output[key] = coordinates_below_threshold
 
     print("Final output with coordinates below the threshold:", final_output)
     return final_output
             
 
-
-def display_images_for_region(all_triplets, region_name):
-    print(f"Displaying images for {region_name}...")
-    triplet_index = 0
-    while triplet_index < len(all_triplets):
-        batch_triplets = all_triplets[triplet_index:triplet_index + 3]
-        show_slices(batch_triplets)  
-        triplet_index += 3
-
-        if triplet_index < len(all_triplets):
-            print(f"Continuing with more slices from {region_name}...")
-
-
-
-
 if __name__ == "__main__":
-
     # Example dictionary holding your image data for skull segmentation
     sitk_images_dict = {
         "image1": data.get_3d_image("scan1"),
         #"image2": data.get_3d_image("scan2"),
     }
-    execute_unet(sitk_images_dict)
+    results = data.set_seg_results_with_dir(r"C:\\Users\\Justin Rivera\\OneDrive\\Documents\\ED1\\Testing Atlas seg Unet.DCMs")
+    final_output = execute_unet(data.segmentation_results)
+    print(final_output)
+    print(results)
